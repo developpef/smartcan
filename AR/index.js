194a2e7a8262c6b32a9b2444849adb0e3a8d50fd
frup43821 <frup43821@gfi.fr>
2018-03-15 17:15:13 +0100

    'test'

---
+++ AR/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles", express.static(__dirname));
+app.use("/data", express.static(__dirname + '/data'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/obj", express.static(__dirname + '/obj'));
+app.use("/stl", express.static(__dirname + '/stl'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
